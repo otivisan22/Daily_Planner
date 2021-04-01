adaae3df8d5128bd72d06ae4a3dde2bc0955419f
Otilia Visan <otilia.visan22@gmail.com>
2021-04-01 01:55:38 +0100

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,23 @@ const renderCurrentDate = () => {
   const displayNow = moment().format("dddd, MMMM Do");
   console.log(displayNow);
 };
+
+const renderCalendarEvents = () => {
+  const plannerEvents = localStorage.getItem("plannerEvents");
+
+  if (plannerEvents !== null) {
+    console.log("render the data in the table");
+  } else {
+    localStorage.setItem("plannerEvents", JSON.stringify({}));
+  }
+};
+
+const onReady = () => {
+  console.log("Ålskä");
+
+  //renderCurrentTime
+
+  renderCalendarEvents();
+};
+
+$(document).ready(onReady);
